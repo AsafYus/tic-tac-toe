df9ffa12308643e102c41b4654fe8cc85a02d093
Asaf Yusufov <ayosopov@gmail.com>
2023-03-30 21:53:06 +0300

    Add const getter function for Player name and prevent copying of Player objects. \ This commit adds a const getter function for the Player name to retrieve the name of the player. \ Additionally, the copy constructor and copy assignment operator have been declared as private in the class \ to prevent copying of Player objects and ensure the class is non-copyable. These changes enhance \ the encapsulation of the Player class and improve its design.

--- Player/Player.h
+++ Player/Player.h
@@ -2,4 +2,31 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <string>       // for std::string
+
+class Player {
+public:
+    virtual ~Player() = default;
+    // Makes a move in the game and returns true if successful, false otherwise.
+    // This function should be implemented by derived classes.
+    virtual bool make_move() = 0;
+    // Checks if the player has won the game and returns true if so, false otherwise.
+    // This function should be implemented by derived classes.
+    virtual bool check_win() = 0;
+
+    const std::string& get_name() const;
+    
+
+protected:
+    Player(const std::string& name);
+    std::string _name;
+
+private:
+    // This private section declares the copy constructor and copy assignment operator as private,
+    // which means they are not accessible from outside the class.
+    // This prevents copying of Player objects and ensures that the class is non-copyable.
+    Player(const Player&);
+    Player& operator=(const Player&);
+};
+
 #endif // PLAYER_H
